7e8f91e044548a7f1471e1c3464cf696f8b8ea7a
Taofeek <taofeekdboy@yahoo.com>
2024-08-31 16:11:13 +0300

    dotenv file expose

--- helpers/defaultConfig.ts
+++ helpers/defaultConfig.ts
@@ -1,4 +1,7 @@
 import { devices, PlaywrightTestConfig } from '@playwright/test';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const defaultConfig: PlaywrightTestConfig = {
 	testDir: './tests',
