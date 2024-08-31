9482eb25f79598f8b8b3c68ac07decc97afeb70c
Taofeek <taofeekdboy@yahoo.com>
2024-08-31 22:53:29 +0300

    testing deepmerge

--- utils/deepMerge.ts
+++ utils/deepMerge.ts
@@ -1,6 +1,6 @@
 import { PlaywrightTestConfig } from '@playwright/test';
 
-export const deepMerge = (target: any, source: any): PlaywrightTestConfig => {
+export const deepMerge = (target: any, source: any): any => {
 	for (const key in source) {
 		if (source[key] instanceof Object) {
 			Object.assign(source[key], deepMerge(target[key], source[key]));
